001a283d2c1ed791046e067dfaf13f2edce5e5c7
carlos.rabelo <carlos.rabelo@wundermanthompson.com>
2023-03-21 18:25:24 +0000

    feat: button

---
+++ src/component/Button/index.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { Theme } from "@emotion/react";
+import styled from "@emotion/styled";
+
+export type Colors = keyof Theme["colors"];
+
+export const Button = styled.button<{ color?: Colors }>`
+  cursor: pointer;
+
+  height: 32px;
+  padding: 4px 15px;
+
+  color: #fff;
+  text-align: center;
+
+  background-color: ${(p) => p.theme.colors[p.color || "primary"].main};
+  background-image: none;
+  border: 1px solid transparent;
+  border-radius: 6px;
+  outline: none;
+
+  transition: background-color ${(p) => p.theme.transition};
+
+  &:hover {
+    color: #fff;
+    background-color: ${(p) => p.theme.colors[p.color || "primary"].dark};
+  }
+`;
